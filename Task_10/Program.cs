db3e632754948f834010941775048e8924b9536f
Kumirena <irshel1@ya.ru>
2022-12-08 01:08:13 +0300

    add Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход
+трехзначное число и на выходе показывает вторую цифру этого числа.
+*/ 
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string n = Convert.ToString(number);
+{
+Console.WriteLine($"Трехзначное число: {number}");
+Console.WriteLine($"Вторая цифра: " +n[1]);
+}
+
+
+
+
+
+
