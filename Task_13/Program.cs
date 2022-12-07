9b4c09fc8da1b73ee1e3761354abc90ad77db8d5
Kumirena <irshel1@ya.ru>
2022-12-08 01:42:07 +0300

    add Task 13 Task 15

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет.
+*/
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strnumber = Convert.ToString(number);
+
+if(strnumber.Length > 2)
+
+{
+        Console.WriteLine($"Третья цифра: " +strnumber[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+*/
+Console.WriteLine("Введите число,обозначающее день недели: ");
+string day = (Console.ReadLine() ?? "");
+switch (day)
+{
+    case "1":
+    Console.WriteLine("Будний день - понедельник - день тяжелый");
+    break;
+
+    case "2":
+    Console.WriteLine("Будний день ");
+    break;
+
+    case "3":
+    Console.WriteLine("Будний день ");
+    break;
+
+    case "4":
+    Console.WriteLine("Будний день ");
+    break;
+
+    case "5":
+    Console.WriteLine("Будний день ");
+    break;
+
+    case "6":
+    Console.WriteLine("Выходной день! Ура!");
+    break;
+
+    case "7":
+    Console.WriteLine("Выходной день! Ура!");
+    break;
+
+    default:
+    Console.WriteLine("Ввели неправильное число");
+    break;
+}
